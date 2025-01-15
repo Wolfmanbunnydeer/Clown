e2537bd5c00dcad8b0a51c3dfa4ba6fcb92700e6
Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>
2025-01-14 19:42:03 -0500

    Update script.js
    Signed-off-by: Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -5,61 +5,79 @@ function AddGame() {
   var homePoint = document.getElementById("homepoint").value;
   var awayPoint = document.getElementById("awaypoint").value;
   
-  var gameDiv = document.getElementById("gameDiv");
   var gameOutput = document.getElementById("gameOutput");
   
+  // Create a new paragraph to show the game info
   var gameInfo = document.createElement("p");
   gameInfo.innerHTML = "Date: " + date + "<br>Home Team: " + homeTeam + "<br>Away Team: " + awayTeam + "<br>Home Points: " + homePoint + "<br>Away Points: " + awayPoint;
   
   gameOutput.appendChild(gameInfo);
-  
-  gameDiv.innerHTML = "";
-  gameDiv.appendChild(document.createElement("br"));
-  gameDiv.appendChild(document.createElement("br"));
-  
-  var newGameForm = document.createElement("form");
-  newGameForm.id = "gamesForm";
-  
-  var newGameSelect = document.createElement("select");
-  newGameSelect.id = "mySelect";
-  newGameSelect.innerHTML = `
-    <option>Arizona Cardinal</option>
-    <option>Atlanta Falcons</option>
-    <option>Carolina Panthers</option>
-    <option>Chicago Bears</option>
-    <option>Dallas Cowboys</option>
-    <option>Detroit Lions</option>
-    <option>Jacksonville Jaguars</option>
-    <option>Green Bay Packers</option>
-    <option>Los Angeles Rams</option>
-    <option>Minnesota Vikings</option>
-    <option>New Orleans Saints</option>
-    <option>New York Giants</option>
-    <option>Denver Broncos</option>
-    <option>Houston Texans</option>
-    <option>Miami Dolphins</option>
-    <option>Tennessee Titans</option>
-    <option>Buffalo Bills</option>
-  `;
-  
-  newGameForm.appendChild(document.createElement("br"));
-  newGameForm.appendChild(document.createElement("br"));
-  
-  var label = document.createElement("label");
-  label.innerHTML = "Please Enter Home Team:";
-  
-  var newGameButton = document.createElement("button");
-  newGameButton.onclick = AddGame;
-  newGameButton.innerHTML = "Add Game";
-  
-  gameDiv.appendChild(newGameForm);
-  newGameForm.appendChild(label);
-  newGameForm.appendChild(newGameSelect);
-  newGameForm.appendChild(newGameButton);
 }
 
-var addButton = document.createElement("button");
-addButton.onclick = AddGame;
+function createForm() {
+  var gameDiv = document.getElementById("gameDiv");
+
+  // Create a form for adding a game
+  var newGameForm = document.createElement("form");
+  newGameForm.id = "gamesForm";
+
+  var labelDate = document.createElement("label");
+  labelDate.innerHTML = "Enter Game Date: ";
+  var inputDate = document.createElement("input");
+  inputDate.id = "date";
+  inputDate.type = "date";
+
+  var labelHomeTeam = document.createElement("label");
+  labelHomeTeam.innerHTML = "Enter Home Team: ";
+  var inputHomeTeam = document.createElement("input");
+  inputHomeTeam.id = "hometeam";
+  
+  var labelAwayTeam = document.createElement("label");
+  labelAwayTeam.innerHTML = "Enter Away Team: ";
+  var inputAwayTeam = document.createElement("input");
+  inputAwayTeam.id = "awayteam";
+
+  var labelHomePoints = document.createElement("label");
+  labelHomePoints.innerHTML = "Enter Home Points: ";
+  var inputHomePoints = document.createElement("input");
+  inputHomePoints.id = "homepoint";
+  inputHomePoints.type = "number";
+
+  var labelAwayPoints = document.createElement("label");
+  labelAwayPoints.innerHTML = "Enter Away Points: ";
+  var inputAwayPoints = document.createElement("input");
+  inputAwayPoints.id = "awaypoint";
+  inputAwayPoints.type = "number";
+  
+  var addButton = document.createElement("button");
+  addButton.type = "button";
+  addButton.onclick = AddGame;
+  addButton.innerHTML = "Add Game";
+
+  // Append elements to the form
+  newGameForm.appendChild(labelDate);
+  newGameForm.appendChild(inputDate);
+  newGameForm.appendChild(document.createElement("br"));
+  newGameForm.appendChild(labelHomeTeam);
+  newGameForm.appendChild(inputHomeTeam);
+  newGameForm.appendChild(document.createElement("br"));
+  newGameForm.appendChild(labelAwayTeam);
+  newGameForm.appendChild(inputAwayTeam);
+  newGameForm.appendChild(document.createElement("br"));
+  newGameForm.appendChild(labelHomePoints);
+  newGameForm.appendChild(inputHomePoints);
+  newGameForm.appendChild(document.createElement("br"));
+  newGameForm.appendChild(labelAwayPoints);
+  newGameForm.appendChild(inputAwayPoints);
+  newGameForm.appendChild(document.createElement("br"));
+  newGameForm.appendChild(addButton);
+
+  // Append the form to the gameDiv
+  gameDiv.appendChild(newGameForm);
+}
+
+window.onload = createForm;  // Call the function to set up the form when the page loads.
+
 addButton.innerHTML = "Add Game";
 
 document.getElementById("gamesForm").appendChild(addButton);
